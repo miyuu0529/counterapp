4ec5065b357832cff90312449c346c714dae8611
miyuu0529 <disney.happy4649@gmail.com>
2020-06-22 11:49:19 +0900

    1st

--- src/router/index.js
+++ src/router/index.js
@@ -17,11 +17,27 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/count',
+    name: 'Count',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Count.vue')
+  },
+   {
+    path: '/score',
+    name: 'Score',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Score.vue')
+  },
+  
 ]
 
 const router = new VueRouter({
-  mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count:0
   },
-  mutations: {
+  
+  mutations:{
+  increment: state => state.count++,
+  decrement: state => state.count--
   },
   actions: {
   },
   modules: {
+  },
+  getters: {
+    cont: state => {return state.count}
   }
 })
